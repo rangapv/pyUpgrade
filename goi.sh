66d2b180f3cb19d8d36ff2a068f111a9513c82b9
rangapv <rangapv@yahoo.com>
2019-01-07 06:05:28 +0000

    support for centos extended ;-)

--- goi.sh
+++ goi.sh
@@ -3,8 +3,8 @@ li=$(uname -s)
 li2="${li,,}"
 
 u1=$(cat /etc/*-release | grep ubuntu)
-f1=$(cat /etc/*-release | grep fedora)
-c1=$(cat /etc/*-release | grep centos)
+f1=$(cat /etc/*-release | grep ID= | grep fedora)
+c1=$(cat /etc/*-release | grep ID= | grep centos)
 s1=$(cat /etc/*-release | grep suse)
 
 
@@ -27,7 +27,7 @@ then
         echo " " >> ~/.bashrc
         echo "export PATH=$PATH:/usr/lib/go-$gover/bin" >> ~/.bashrc
         echo "export GOPATH=~/go" >> ~/.bashrc
-        echo "GO Installed Pls logout/login or in a new shell to test type \"go version\" "
+        echo "GO Installed Pls logout re-login or in a new shell to test type \"go version\" "
 fi
 
 
@@ -46,12 +46,23 @@ then
         echo " " >> ~/.bashrc
         echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
         echo "export GOPATH=~/go" >> ~/.bashrc
-        echo "GO Installed Pls logout/login or in a new shell to test type \"go version\" "
+        echo "GO Installed Pls logout re-login or in a new shell to test type \"go version\" "
 fi
 
 if [ ! -z "$c1" ]
 then
-	echo "it is a centos"
+	echo "IT IS CENTOS"
         ji=$(cat /etc/*-release | grep '^ID=' |awk '{split($0,a,"\"");print a[2]}')
         ki="${ji,,}"
+	echo " What version of go is required for Fedora 1.7/1.8/1.9/1.10/1.11 "
+        read gover
+        sudo yum -y update
+        sudo yum -y install wget
+        wget https://storage.googleapis.com/golang/go$gover.linux-amd64.tar.gz
+        tar -xvf go$gover.linux-amd64.tar.gz
+        sudo mv go /usr/local
+	echo " " >> ~/.bashrc
+        echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
+        echo "export GOPATH=~/go" >> ~/.bashrc
+        echo "GO Installed Pls logout re-login or in a new shell to test type \"go version\" "
 fi
