b160e366cd779cbe8b5cfc29576c670f633b865f
rangapv <rangapv@yahoo.com>
2019-05-26 08:34:46 +0000

    Better functions

--- py.sh
+++ py.sh
@@ -24,40 +24,41 @@ then
         pi1="${pi,,}"
         f1=$(>t2.txt)
         mod=$(chmod 777 t2.txt)
-        python -V > ./t2.txt 2>&1
-        python3 -V > ./t3.txt 2>&1
+	piver=$(python -V 2>&1)
+	piver1=$( echo "${piver}" | awk '{split($0,a," ");print a[2]}')
+	piver12=$( echo "${piver1}" | awk '{split($0,a,".");print a[1]}')
+	piver2=$(python2 -V 2>&1)
+	piver21=$( echo "${piver2}" | awk '{split($0,a," ");print a[2]}')
+	piver22=$( echo "${piver21}" | awk '{split($0,a,".");print a[1]}')
 	piver3=$(python3 -V 2>&1)
 	piver31=$( echo "${piver3}" | awk '{split($0,a," ");print a[2]}')
 	piver32=$( echo "${piver31}" | awk '{split($0,a,".");print a[1]}')
-	echo "P1 + ${piver3}"
-	echo "P2 + ${piver31}"
-	echo "P3 + ${piver32}"
-	pyver=$(cat ./t2.txt | awk '{split($0,a," ");print a[2]}') 
-        echo "${pyver}"
-        piver=$( echo "${pyver}" | awk '{split($0,a,".");print a[1]}')
-        echo "${piver}"
+
+	case ${piver12} in
+		2)
+			echo "Python Version 2 upgrading to 3"
+			sudo ln -sf /usr/bin/python3 /usr/bin/python
+         		sudo apt-get -y upgrade
+         		sudo apt-get install -y python3-pip
+         		sudo pip3 install --upgrade pip
+         		sudo pip3 install awscli
+         		sudo pip3 install boto
+         		sudo pip3 install boto3	
+		;;
+		3)
+		 	echo "Python Version 3"
+			sudo ln -sf /usr/bin/python3 /usr/bin/python
+         		sudo apt-get -y upgrade
+         		sudo apt-get install -y python3-pip
+         		sudo pip3 install --upgrade pip
+         		sudo pip3 install awscli
+         		sudo pip3 install boto
+         		sudo pip3 install boto3	
+		;;
+		*)
+			echo "No Python Installed in this BOX"
+		;;
+	esac
        # echo "$(( $pyver / 3 ))"
-      
-       if [ "${piver}" == "2" ] || [ "${piver32}" == "3" ]
-       then
-         echo "BINGO"
-         sudo ln -sf /usr/bin/python3 /usr/bin/python
-         sudo apt-get -y upgrade
-         sudo apt-get install -y python3-pip
-         sudo pip3 install --upgrade pip
-         sudo pip3 install awscli
-         sudo pip3 install boto
-         sudo pip3 install boto3
-      
-       elif [ "${piver32}" == "3" ]
-       then
-         echo "BINGO it is 3"
-         sudo pip3 install awscli
-         sudo pip3 install boto
-         sudo pip3 install boto3
-         sudo ln -sf /usr/bin/python3 /usr/bin/python
-       else
-         echo "Python Not installed"
-       fi
 
 fi
